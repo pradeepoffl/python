532d898d4b10130d7f29095d7e782311965bec08
pradeepoffl <k.b.v.pradeepkumar@gmail.com>
2024-10-26 10:50:03 +0530

    commited

---
+++ 5-Modules/test.py
@@ -0,0 +1,9 @@
+from package.maths import *
+from package.subpackage.power import *
+# import  all functions from maths module
+
+print(addition(4,55))
+print(substraction(40,29))
+print(power(2,4))
+
+# if you want to run you should in 5module folder
